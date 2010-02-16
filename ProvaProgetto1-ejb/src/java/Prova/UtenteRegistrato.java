2f0b9b4f2460610feec4fd0dd069c86c89811084
dani1913 <dani1913@gmail.com>
2010-02-16 18:30:53 +0000

    comment added

--- ProvaProgetto1-ejb/src/java/Prova/UtenteRegistrato.java
+++ ProvaProgetto1-ejb/src/java/Prova/UtenteRegistrato.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.Id; //libreria per chiavi esterne di tipo semplice
 
 /**
  *
