d1dd7c6d505c1f2277311af3890f1175b4499e48
dani1913@gmail.com <dani1913@gmail.com@c1827090-1afb-11df-97a5-0fcb8bdf4c86>
2010-05-19 19:46:26 +0000

    facebook

--- trunk/SviluppoSoftware/SviluppoSoftware-ejb/src/java/prova/GestoreLineaMagazzinoBean.java
+++ trunk/SviluppoSoftware/SviluppoSoftware-ejb/src/java/prova/GestoreLineaMagazzinoBean.java
@@ -43,74 +43,56 @@ public class GestoreLineaMagazzinoBean implements GestoreLineaMagazzinoBeanLocal
     private Queue queue;
 
     public void checkQuantita(Prenotazione p, PrintWriter ot){
-        String zona = p.getZona();
-        Long idMagazzino;
-        Long id_materia;
-        int quantitaMinima;
-        int quantita;
-        int n_rifornimenti;
-        ArrayList<String> listaMaterie;
-        ConfigurazionePiatto[] piatti = p.getMappaPiatti().values().toArray(new ConfigurazionePiatto[p.getMappaPiatti().size()]);
-
-       for(int i=0;i<p.getMappaPiatti().size();i++){
-            listaMaterie= piatti[i].materiePrime();
-            ot.println(listaMaterie);
-            for(int j=0;j<listaMaterie.size();j++){
-                LineaMagazzino o = (LineaMagazzino)lineaMagazzinoFacade.findCheckMateria(listaMaterie.get(j), zona).get(0);
-                id_materia = o.getMatPrima().getId();
-                idMagazzino = o.getMag().getId();
-                quantitaMinima = o.getSogliaMinima();
-                quantita = o.getQuantita();
-
-                o.setQuantita(quantita-piatti[i].getQnt());
-
-                lineaMagazzinoFacade.edit(o);
-                n_rifornimenti = o.getN_rif();
-
-                if(quantita<quantitaMinima)
-                 contattaFornitori(zona,idMagazzino,n_rifornimenti,id_materia);
-            }
-        }
-
-    }
-
-
-    private void contattaFornitori(String zona,Long id_Magazzino,int n_rifornimenti,Long materia){
         try{
-
-/*            List<LineaMagazzino> lista = lineaMagazzinoFacade.findAll();
-      for(LineaMagazzino linea:lista)
-      {if((linea.getMag().getId().equals(id_Magazzino))&&(linea.getMatPrima().getId().equals(materia)))
-       {linea.setQuantita(linea.getQuantita()+n_rifornimenti);
-        lineaMagazzinoFacade.edit(linea);
-       }
-      }
-*/
-
-
             Connection connection = connectionFactory.createConnection();
             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            MessageConsumer mc = session.createConsumer(queue);
             MessageProducer messageProducer = session.createProducer(queue);
 
+            Long idMagazzino;
+            Long id_materia;
+            int quantitaMinima;
+            int quantita;
+            int n_rifornimenti;
+            ArrayList<String> listaMaterie;
+            String zona = p.getZona();
+            ConfigurazionePiatto[] piatti = p.getMappaPiatti().values().toArray(new ConfigurazionePiatto[p.getMappaPiatti().size()]);
 
-            ObjectMessage message = session.createObjectMessage();
-            message.setLongProperty("id_mag", id_Magazzino);
-            message.setLongProperty("id_materia", materia);
-            message.setStringProperty("zona", zona);
-            message.setIntProperty("rifornimenti", n_rifornimenti);
+            for(int i=0;i<p.getMappaPiatti().size();i++){
+                listaMaterie= piatti[i].materiePrime();
+                ot.println(listaMaterie);
 
+                for(int j=0;j<listaMaterie.size();j++){
+                    LineaMagazzino o = (LineaMagazzino)lineaMagazzinoFacade.findCheckMateria(listaMaterie.get(j), zona).get(0);
+                    id_materia = o.getMatPrima().getId();
+                    idMagazzino = o.getMag().getId();
+                    quantitaMinima = o.getSogliaMinima();
+                    quantita = o.getQuantita();
+                    n_rifornimenti = o.getN_rif();
 
-            messageProducer.send(message);
+                    o.setQuantita(quantita-piatti[i].getQnt());
 
+                    lineaMagazzinoFacade.edit(o);
 
-            MessageConsumer mc = session.createConsumer(queue);
-
-
+                    if(quantita<quantitaMinima){
+                           //contattaFornitori(zona,idMagazzino,n_rifornimenti,id_materia);
+                        ObjectMessage message = session.createObjectMessage();
+                        message.setLongProperty("id_mag", idMagazzino);
+                        message.setLongProperty("id_materia", id_materia);
+                        message.setStringProperty("zona", zona);
+                        message.setIntProperty("rifornimenti", n_rifornimenti);
+                        messageProducer.send(message);
+                    }
+                }
+            }
             messageProducer.close();
             connection.close();
             mc.close();
         }catch(Exception e){}
     }
+
+
+
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method" or "Web Service > Add Operation")
